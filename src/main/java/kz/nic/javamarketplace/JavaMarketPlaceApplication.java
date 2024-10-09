ca90ff06f79942c9c5d418a6cd981517287a7c94
Farukh <excommunicad00@yandex.ru>
2024-10-10 00:43:28 +0500

    Initial commit with all files

---
+++ src/main/java/kz/nic/javamarketplace/JavaMarketPlaceApplication.java
@@ -0,0 +1,13 @@
+package kz.nic.javamarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMarketPlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMarketPlaceApplication.class, args);
+    }
+
+}
